6b275329b29a98fcd7f56d9cc551250668e28617
Anna Zubareva <a.zubareva2@g.nsu.ru>
2025-10-06 21:05:43 +0700

    9 task

---
+++ 9.py
@@ -0,0 +1,7 @@
+ATT = int(input())
+COMP = int(input())
+YDS = int(input())
+TD = int(input())
+INT = int(input())
+passer_rating = ((COMP/ATT - 0.3) * 5 + (YDS/ATT - 3) * 0.25 + (TD/ATT) * 20 + 2.375 - (INT/ATT * 25)) / 6 * 100
+print(passer_rating)
